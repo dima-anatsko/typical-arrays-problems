1ca9c58e856026acd62e301dcf37cdada201ea72
Dmitry Anatsko <anatskodr@gmail.com>
2021-03-09 23:28:45 +0300

    feat: add task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  if (array.length != 0) {
+  let minValue = array.sort(function(a, b) { return a - b; });
+  return minValue[0];
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  if (array.length != 0) {
+    let maxValue = array.sort(function(a, b) { return a - b; });
+    return maxValue[maxValue.length - 1]
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  if (array.length != 0) {
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
+  } else {
+    return 0;
+  }
 }
